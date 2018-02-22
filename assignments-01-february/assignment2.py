073c90e9b0be9e50ee3d19e7c5694476f70ff1b2
David Fokkema <davidfokkema@gmail.com>
2018-02-22 12:13:55 +0100

    Live plot some data
    My god, this is slow. If i change the delay to something close to zero,
    python burns up a lot of cpu and still takes many seconds to plot 100
    points.

--- assignments-01-february/assignment2.py
+++ assignments-01-february/assignment2.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt
 import time
 import random
+import numpy as np
 
+xsample = np.linspace(0, 100, 100)
 ysample = random.sample(range(-50, 50), 100)
 xdata = []
 ydata = []
 plt.show()
 
+# not necessary wth ipython --pylab
+#plt.ion()
+
 axes = plt.gca()
 axes.set_xlim(0, 100)
 axes.set_ylim(-50, +50)
-line, = axes.plot(xdata, ydata, 'r-') #note the comma after line!!!
-maxt = 100
+
+for x, y in zip(xsample, ysample):
+    xdata.append(x)
+    ydata.append(y)
+    line, = axes.plot(xdata, ydata, 'r-') #note the comma after line!!!
+    plt.pause(.1)
