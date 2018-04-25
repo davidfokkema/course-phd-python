01de9d3e6d5e43b8c2d5fcd929af4792471874f6
David Fokkema <davidfokkema@gmail.com>
2018-04-25 22:42:57 +0200

    Create threads managed by Qt

--- assignments-04-april/daq_gui.py
+++ assignments-04-april/daq_gui.py
@@ -2,8 +2,9 @@
 
 import multiprocessing
 import sys
+import time
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 import pyqtgraph as pg
 
 from fake_daq import DataAcquistion
@@ -15,12 +16,25 @@ class UserInterface(QtWidgets.QWidget):
         super().__init__()
 
         self.queue = queue
+        self.init_worker()
         self.init_ui()
+        self.start_daq()
 
-    def run(self):
-        while True:
-            data = self.queue.get()
-            print(data)
+    def init_worker(self):
+        """Setup the worker thread.
+
+           First, instantiate the worker and a QThread. Then, make sure the
+           worker will run in a thread managed by the QThread by calling the
+           :meth:`QObject.moveToThread` method. Finally, attach the `started`
+           signal to the `run` slot on the worker, so that the worker will
+           actually start.
+
+        """
+
+        self.daq_worker = DAQWorker()
+        self.daq_thread = QtCore.QThread()
+        self.daq_worker.moveToThread(self.daq_thread)
+        self.daq_thread.started.connect(self.daq_worker.run)
 
     def init_ui(self):
         """Create the user interface."""
@@ -37,13 +51,31 @@ class UserInterface(QtWidgets.QWidget):
         self.setLayout(layout)
         self.show()
 
+    def start_daq(self):
+        self.daq_thread.start()
+
+    def stop_daq(self):
+        self.daq_thread.stop()
+
+    def read_data(self):
+        while True:
+            data = self.queue.get()
+            print(data)
+
+
+class DAQWorker(QtCore.QObject):
+
+    @QtCore.pyqtSlot()
+    def run(self):
+        while True:
+            print("Running...")
+            time.sleep(1)
+
 
 if __name__ == '__main__':
     qtapp = QtWidgets.QApplication(sys.argv)
 
     queue = multiprocessing.Queue()
-    daq = DataAcquistion(queue)
     ui = UserInterface(queue)
 
-    daq.start()
     qtapp.exec_()
